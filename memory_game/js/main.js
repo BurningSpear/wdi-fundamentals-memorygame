df4a2867291bed78d0a97e9778b2c675e7e19d0d
frank kigenyi <kigenyifrank@gmail.com>
2020-01-17 08:23:14 -0600

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,42 +1,132 @@
-const numberOfCards = 2;
-const cards = ["queen", "queen", "king", "king"];
-const cardsInPlay = [];
-//checking for a matching cards if cardsInPlay
- function checkForMatch(){
- //logic to check if 2 cads are in play
-		if (cardsInPlay.length === 2){
-		//check for match
-			if (cardsInPlay[0] === cardsInPlay[1]) {
-				alert("Hippie Ya!!!. Dude, Yo Cards are great");
-				playerScore++;
-				scoreDisplay.textContent = playerScore;
-			} else {
-				alert("Hey dude, not yo day tody. Try again, Loser!!!");
-			}
-		}
-	}
-// logic when users flips a card and checkForMatch
-function flipCard(cardId) {
-cardsInPlay.push(cards[cardId]);
-	if(cardsInPlay.length === 2) {
-		checkForMatch();
-		console.log("User flipped" + cards[cardId]);
-	}	
-};
 
+//const cards["queen", "queen", "king", "king"];
+//create new cardsInPlay objects Array
 
-/*
-const cardOne = cards[0];
-cardsInPlay.push(cardOne);
-
-const cardTwo = cards[1];
-cardsInPlay.push(cardTwo);
-
-if (cardsInPlay.length === numberOfCards) { 	
-	if (cardsInPlay[0] === cardsInPlay[1]) { 
-   alert ("Hippie Ya!!!. Dude, Yo Cards are great");
-}
-else alert (" Hey dude, try again. Loser!!!")
-}
-console.log("User flipped " + cardsInPlay);
-*/
+let cards = [ 
+ 	{ 
+ 		  rank : "queen" 
+ 		, suit : "hearts" 
+ 		, cardImage: "images/queen-of-hearts.png" 
+ 	}, 
+ 	 
+	{ 
+ 		  rank : "queen" 
+ 		, suit : "diamonds" 
+ 		, cardImage: "images/queen-of-diamonds.png" 
+ 	}, 
+ 	 
+ 	{ 
+ 		  rank : "king" 
+ 		, suit : "hearts" 
+ 		, cardImage: "images/king-of-hearts.png"
+ 	}, 
+ 	 
+ 	{ 
+ 		  rank : "king" 
+ 		, suit : "diamonds" 
+ 		, cardImage: "images/king-of-diamonds.png" 
+ 	}	 
+ ]; 
+ 
+ 
+ let cardsInPlay = []; 
+ let gamesPlayed = 0; 
+ let gamesWon = 0; 
+ let playAgain = true; 
+ let tempCardId = ""; 
+ 
+ 
+ function checkForMatch() { 
+ 	if (cardsInPlay[0] === cardsInPlay[1]) { 
+ 		document.getElementById('message').textContent = "You found a match!"; 
+ 		gamesWon++; 
+ 	} 
+ 	else 
+ 		document.getElementById('message').textContent = "Sorry, try again."; 
+ 	 
+ 	gamesPlayed++; 
+ 	 
+ 	// Update score 
+ 	document.getElementById('gamesPlayed').textContent = "Games Played: " +  
+     	gamesPlayed; 
+ 	 
+ 	document.getElementById('gamesWon').textContent = "Games Won: " + gamesWon; 
+ 		 
+ } 
+ 
+ 
+ function flipCard() { 
+ 	 
+ 	if (playAgain === false) return; 
+ 	 
+ 	let cardId = this.getAttribute('data-id'); 
+ 	 
+ 	// Make sure we aren't cheating by flipping the same card! 
+ 	if (tempCardId === cardId)  
+ 		return; 
+ 	else 
+ 		tempCardId = cardId; 
+ 		 
+ 	cardsInPlay.push(cards[cardId].rank); 
+ 	 
+ 	this.setAttribute('src', cards[cardId].cardImage); 
+ 	 
+ 	if (cardsInPlay.length === 2) { 
+ 		playAgain = false; 
+ 		checkForMatch(); 
+ 	} 
+ 	 
+ } 
+ 
+ 
+ function createBoard() { 
+ 		 
+ 	// Shuffling cards array 
+ 	shuffle(cards); 
+ 	 
+ 	document.getElementById('message').innerHTML = "&nbsp;"; 
+ 	 
+ 	// Clear the board 
+ 	const myNode = document.getElementById('game-board'); 
+ 	while (myNode.firstChild) 
+ 		myNode.removeChild(myNode.firstChild); 
+ 	 
+ 	// Create a new board 
+ 	for(let i = 0; i < cards.length; i++) { 
+ 		 
+ 		let cardElement = document.createElement('img'); 
+ 		cardElement.setAttribute('src', "images/back.png"); 
+ 		cardElement.setAttribute('data-id', i); 
+ 		cardElement.addEventListener('click', flipCard); 
+ 		document.getElementById('game-board').appendChild(cardElement); 
+ 	} 
+ } 
+ 
+ 
+ function resetBoard() { 
+ 	// Create a new board 
+ 	cardsInPlay = []; 
+ 	createBoard(); 
+ 	playAgain = true; 
+ 	tempCardId = ""; 
+ } 
+ 
+ 
+ 
+ 
+ // From https://javascript.info/task/shuffle 
+ function shuffle(array) { 
+ 	  for (let i = array.length - 1; i > 0; i--) { 
+ 	    let j = Math.floor(Math.random() * (i + 1)); // random index from 0 to i 
+ 
+ 
+ 	    // swap elements array[i] and array[j] 
+ 	    // let us use "destructuring assignment" syntax to achieve that 
+ 	    // same can be written as: 
+ 	    // let t = array[i]; array[i] = array[j]; array[j] = t 
+ 	    [array[i], array[j]] = [array[j], array[i]]; 
+ 	  } 
+ 	} 
+ 
+ 
+ createBoard(); 
